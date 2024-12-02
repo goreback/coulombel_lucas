26ddb35059dc83aceac0313bf9fcc87d287fd3ce
Mostapha <mostapha-bachir.addi@redstoneformations.fr>
2024-12-02 13:40:27 +0100

    Added routes and project structure

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express, {Request, Response} from 'express';
 import mongoose from 'mongoose';
-import User  from './models/User';
+import userRoutes from './routes/userRoutes';
 
 
 
@@ -11,6 +11,7 @@ const PORT = 3000;
 
 app.use(express.json());
 
+app.use('/api', userRoutes);
 
 
 const uri = "mongodb://root:example@localhost:27017/";
@@ -21,41 +22,6 @@ mongoose.connect(uri)
     .catch((error) => {
         console.log('Error connecting to MongoDB', error)
     })
-
-// Route par défaut
-app.get('/', (req: Request, res: Response) => {
-    res.send('balbflasblfknag');
-})
-
-// Route pour créer un utilisateur
-app.post('/users', async (req: Request, res: Response): Promise<any> => {
-    try {   
-    const {name, email, age} = req.body;
-
-    if (!name || !email || !age) {
-        return res.status(400).json({
-            message: 'Name, email and age are required'
-        })
-    }
-    const newUser = new User({name, email, age});
-    const savedUser = await newUser.save();
-    res.status(201).json({
-        message: 'User created successfully',
-        user: savedUser})
-}
-    catch (error: any) {
-        if (error.code === 11000) {
-            return res.status(409).json({
-                message : 'Cette adresse email existe deja'
-            })
-        }
-        console.log(error)
-        return res.status(500).json({
-            message: 'Error creating user',
-            error: error.message
-        })
-}})
-
 
 
 
